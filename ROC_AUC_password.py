3a2e6667e8cb277fe9a3b5094548317cd5493ebb
Oswaldo Mendizabal <ilhuicatzi124mendizabal@gmail.com>
2024-09-12 09:56:21 -0600

    modifies k 10

--- ROC_AUC_password.py
+++ ROC_AUC_password.py
@@ -15,8 +15,8 @@ y = datos['usuario'].to_numpy(dtype=str)
 # Dividir los datos en conjunto de entrenamiento (80%) y prueba (20%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Inicializar el modelo KNN con el mejor k encontrado previamente (14)
-knn = KNeighborsClassifier(n_neighbors=14)
+# Inicializar el modelo KNN con el mejor k encontrado previamente (10)
+knn = KNeighborsClassifier(n_neighbors=10)
 
 # Entrenar el modelo
 knn.fit(X_train, y_train)
